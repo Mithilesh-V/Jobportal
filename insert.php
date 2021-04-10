7de2c064aef0b9e37ba3966b99133702aeb01fa0
Joel-Marc <joel.marceline3@gmail.com>
2021-04-10 18:26:28 +0530

    adding

--- insert.php
+++ insert.php
@@ -10,12 +10,19 @@ try {
     
     $name = $_POST['username'];
     $password = $_POST['password'];
-
-    $sql = "INSERT INTO register
-    VALUES ('$name','$password')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    $txt = "New record created successfully";
+    $conp = $_POST['confirm_password'];
+    if(strcmp($password,$conp)==0)
+    {
+      $sql = "INSERT INTO register
+      VALUES ('$name','$password')";
+      // use exec() because no results are returned
+      $conn->exec($sql);
+      $txt = "New record created successfully";
+    }
+    else
+    {
+      open_window('register.html');
+    }
   } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
   }
---
+++ logina.php
@@ -0,0 +1,33 @@
+<?php
+require_once "config.php";
+
+function open_window($url){
+ header("Location: $url");
+ exit();
+};
+
+try {
+    
+    $name = $_POST['username'];
+    $password = $_POST['password'];
+    $cnt=0;
+    
+    $sql = "SELECT * FROM register 
+    WHERE name='$name' AND password='$password' ";
+
+    foreach ($conn->query($sql) as $row) {
+        $cnt += 1;
+    }
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  if( $cnt == 0)
+  {
+    open_window('index.html');
+  }
+  else
+  {
+      open_window('admin.html');
+  }
+  $conn = null;
+?>
