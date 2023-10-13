c2717f44f86f6d0d5347e364ab20bc8f7a23fffd
Mithilesh V <62344690+Mithilesh-V@users.noreply.github.com>
2023-10-13 10:10:10 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,22 +1,16 @@
 <?php
 
-/*
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'postgres');
-define('DB_PASSWORD', 'Joelmkarthick3');
-define('DB_NAME', 'mydata');
- */
+
 $host = "localhost";
 $user = "postgres";
 $pass = "Joelmkarthick3";
 $db = "mydata";
 
 try{
-    //$conn = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    
     $conn = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
-?>
